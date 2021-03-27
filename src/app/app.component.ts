331f7f0d68adafb936a99313b6f5d49ecb1a33ec
saumya <saumyasinha38@gmail.com>
2021-03-27 13:27:48 +0530

    Hall exam room project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  num : number = 0 
   title = 'first-assignment';
+  dynamicRows: any[] = [];
+  dynamicInputRows: any[] = [];
+  currDiv: string = '';
+      
+  
+  addNew(event) {
+    this.dynamicRows.push(event.target.value);
+  }
+  addInput(){
+    this.dynamicInputRows.push(1);
+  }
+  ShowDiv(divVal: string) {
+    this.currDiv = divVal;
+  }
 }
